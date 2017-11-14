99f25a7dbb220f33d6ebde73b6f926c16a459a0e
Chris Hiestand <chrishiestand@gmail.com>
2017-11-13 17:49:33 -0800

    fade site in more quickly

--- webroot/js/site.js
+++ webroot/js/site.js
@@ -47,8 +47,8 @@ $(document).ready(function () {
 /* Preloader and animations */
 $(window).load(function () { // makes sure the whole site is loaded
 	$('#status').fadeOut(); // will first fade out the loading animation
-	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-	$('body').delay(350).css({'overflow-y': 'visible'});
+	$('#preloader').delay(30).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	$('body').delay(30).css({'overflow-y': 'visible'});
 
 	/* WOW Elements */
 	if (typeof WOW == 'function') {
